97e76768c371ae37f2e3606a44a990ecdbbfb066
Logan Negley <logannegley@gmail.com>
2019-10-11 22:33:40 -0500

    fixed arrays

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -139,9 +139,7 @@ graduates.forEach(item => {
 console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
-// const unisWithUni = graduates.filter(item =>
-//   );
-
+// const unisWithUni = [];
 // console.log(unisWithUni);
 
 // ==== ADVANCED Array Methods ====
